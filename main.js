ffe821905af0257df58ca7fe5365d38fde926a62
David Zeff <dpzeff@gmail.com>
2026-02-12 19:25:49 +0200

    .

--- main.js
+++ main.js
@@ -82,6 +82,7 @@ const content = {
   ml: {
     guide: () => fetch("/ml/guide.html").then((r) => r.text()),
     basics: () => fetch("/ml/basics.html").then((r) => r.text()),
+    trees: () => fetch("/ml/trees.html").then((r) => r.text()),
   },
 };
 
